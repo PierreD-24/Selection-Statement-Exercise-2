01b25231eca8c9c170c3db66cf2c5ae6095d5480
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-20 12:16:27 -0700

    Finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math can be hard good luck!");
+                    break;
+
+                case "Science":
+                    Console.WriteLine("Science is cool, have fun experimenting.");
+                    break;
+
+                case "History":
+                    Console.WriteLine("History is filled with lots of interesting stories.");
+                    break;
+
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    break;
+
+            }
         }
     }
 }
